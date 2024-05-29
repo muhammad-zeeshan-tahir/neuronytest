44f2feea4da56f5c1f91fe30a0440c98b604b96b
Muhammad Zeeshan Tahir <muhammad.zeeshan.tahir93@gmail.com>
2024-05-29 17:33:30 +0500

    changed variable name to most suited

--- app/Repositories/Company/CompanyRepository.php
+++ app/Repositories/Company/CompanyRepository.php
@@ -12,5 +12,5 @@ use Prettus\Repository\Contracts\RepositoryInterface;
 interface CompanyRepository extends RepositoryInterface
 {
     public function chargeWallet(int $company_id, $charge_coins) : bool;
-    public function creditWallet(int $company_id, $charge_coins) : bool;
+    public function creditWallet(int $company_id, $credit_coins) : bool;
 }
