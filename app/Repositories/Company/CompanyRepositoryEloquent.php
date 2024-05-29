df89ee7d7d6a47be881b5d8b12a0a4bdffdfe46f
Muhammad Zeeshan Tahir <muhammad.zeeshan.tahir93@gmail.com>
2024-05-29 18:17:00 +0500

    update comments on functions

--- app/Repositories/Company/CompanyRepositoryEloquent.php
+++ app/Repositories/Company/CompanyRepositoryEloquent.php
@@ -31,6 +31,10 @@ class CompanyRepositoryEloquent extends BaseRepository implements CompanyReposit
         $this->pushCriteria(app(RequestCriteria::class));
     }
 
+    /*
+     * This function minus mentioned coins from wallet.
+     * It is called when hiring manager has to contact the candidate
+     */
     public function chargeWallet(int $company_id, $charge_coins): bool
     {
         $company_coins = Company::find($company_id)->wallet()->first()->coins;
@@ -44,12 +48,16 @@ class CompanyRepositoryEloquent extends BaseRepository implements CompanyReposit
         return false;
     }
 
-    public function creditWallet(int $company_id, $charge_coins): bool
+    /*
+     * This function add more coins to company wallet.
+     * This function is called when
+     */
+    public function creditWallet(int $company_id, $credit_coins): bool
     {
         $company_coins = Company::find($company_id)->wallet()->first()->coins;
 
-        if ($company_coins > $charge_coins) {
-            $wallet_coins = $company_coins + $charge_coins ;
+        if ($company_coins >= 0) {
+            $wallet_coins = $company_coins + $credit_coins ;
 
             return Company::find($company_id)->wallet()->update(['coins' => $wallet_coins]);
         }
