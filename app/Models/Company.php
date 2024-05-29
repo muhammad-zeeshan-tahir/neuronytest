214c5253a2600adf0107552c3be07c9be7db47be
Muhammad Zeeshan Tahir <muhammad.zeeshan.tahir93@gmail.com>
2024-05-29 16:19:41 +0500

    Add wallet hasone relationship in company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Company extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Wallet Relation
+     *
+     * @return hasOne
+     */
+    public function wallet(): hasOne
+    {
+        return $this->hasOne(Wallet::class, 'company_id');
+    }
 }
