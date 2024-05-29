537a9a49c98140751b011093e3195e5f8091f848
Muhammad Zeeshan Tahir <muhammad.zeeshan.tahir93@gmail.com>
2024-05-29 16:20:27 +0500

    Create Candidate Repository

---
+++ app/Repositories/Candidate/CandidateRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories\Candidate;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface StarWarMovieRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface CandidateRepository extends RepositoryInterface
+{
+    //
+}
